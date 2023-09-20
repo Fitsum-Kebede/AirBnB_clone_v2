e77c55b3c6ef3147d2575f57d4b835a20ecd36ca
fitsum-kebede <fitsuminfome@gmail.com>
2023-09-20 11:58:30 -0700

    AirBnB_clone_v2

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
-""" tests the review class """
-from models.base_model import BaseModel
+""" """
+from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
-import pep8
 
 
-class Test_pep8(unittest.TestCase):
-    """pep8 test cases class"""
-    def test_pep8_conformance(self):
-        """Test that we conform to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/review.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-
-class test_review(BaseModel):
-    """ tests the review class """
+class test_review(test_basemodel):
+    """ """
 
     def __init__(self, *args, **kwargs):
         """ """
